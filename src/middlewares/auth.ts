cd23deb3e0cbff2720bb279880275a10257a20f3
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2025-10-16 09:28:12 +0200

    disable require auth temporarily

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -69,6 +69,7 @@ export async function optionalAuth(c: Context, next: Next) {
 }
 
 export async function requireAuth(c: Context, next: Next) {
+  return next() //TODO: Fix when frontend auth is established
   await withSupabase(c, async () => {});
   const user = c.get("user");
   if (!user) {
